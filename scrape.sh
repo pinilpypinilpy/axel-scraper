0ae7e28140ff589644db3557ff7a06273232ff4b
pinilpypinilpy <37025994+pinilpypinilpy@users.noreply.github.com>
2020-09-16 15:44:01 -0300

    added checks for axel and wget, now requires root access to install
    todo: make a new folder within the "downloads" directory for every download, hopefully with the url of the sourced files' site, though may conflict if downloading from the same site twice..

--- scrape.sh
+++ scrape.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root (sudo bash scrape.sh -link here- should do it"
+  exit
+fi
+
+if ! command -v wget &> /dev/null
+then
+    echo "wget appears to not be installed, installing" && sudo apt install wget 
+fi
+
+if ! command -v axel &> /dev/null
+then
+    echo "axel appears to not be installed, installing" && sudo apt install axel
+fi
+
+if [ $# -eq 0 ]
+  then
+    echo "Please add a link: ex. sudo bash scrape.sh http://example.com/directory"
+    exit
+fi
+
 mkdir -p downloads
 mkdir -p wget-temp
 cp filter.awk wget-temp/
@@ -8,6 +30,6 @@ echo "Starting Download"
 cd ..
 while read url; do axel -n 10 -o downloads/ $url; done < wget-temp/links.txt
 #cleanup
-rmdir wget-temp
+rm -r  wget-temp
 echo "Download Complete!"
 
